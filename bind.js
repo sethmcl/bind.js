f5afdd32ec15333d6e15b7c6712e006636733ac3
Seth McLaughlin <s@sethmcl.com>
2013-08-08 11:11:41 -0700

    New API and tests

--- bind.js
+++ bind.js
@@ -2,25 +2,41 @@
 
   w.bind = new Bind();
 
-  function Bind() {
-    this.models = {};
+  function Bind() {}
 
-  }
+  /**
+   * Create a template function that can be called with a data context
+   * @param {HtmlElement} node the html element to use as the template root
+   * @returns {function} template function
+   */
+  Bind.prototype.template = function (node) {
+    var datums = this.parseNode(node);
 
-  Bind.prototype.parseNode = function (node) {
+    return function (context) {
+      this.render(datums, context);
+    }.bind(this);
+  };
+
+  /**
+   * Parse a DOM node, and keep track of all
+   * template variables found in text nodes
+   * @param {HtmlElement} node the node to parse
+   * @param {Array} datums the variable data, used when called recursively
+   * @returns {Array} array of template variables
+   */
+  Bind.prototype.parseNode = function (node, datums) {
     var textNode = document.TEXT_NODE,
         elementNode = document.ELEMENT_NODE,
+        datums = datums || [],
         children,
         i,
-        len,
-        datums = [];
+        len;
 
     if (node.nodeType === textNode) {
       return this.parseTextNode(node);
     }
 
     if (node.nodeType === elementNode) {
-      datums.push(this.parseElementNode(node));
 
       if (node.hasChildNodes) {
         children = Array.prototype.slice.call(node.childNodes);
@@ -29,48 +45,125 @@
           datums.push(this.parseNode(children[i]));
         }
       }
+
+      return this.parseElementNode(node, datums);
     }
 
     return datums;
-  }
+  };
 
-  Bind.prototype.parseElementNode = function (node) {
+  /**
+   * Parse an element node. Loop through attributes, looking for
+   * template variables.
+   * @param {HtmlElement} node the node to parse
+   * @param {Array} datums the template variable data array
+   * @returns {Array} array of template variables
+   */
+  Bind.prototype.parseElementNode = function (node, datums) {
     var attributes = node.attributes,
         attribute,
         i,
         len,
-        datums = [];
+        datums = datums || [],
+        datum;
 
     for (i = 0, len = attributes.length; i < len; i++) {
-      datums.push(this.parseTextNode(attributes[i]));
+      datum = this.parseTextNode(attributes[i]);
+
+      if (datum) {
+        datums.push(datum);
+      }
     }
 
     return datums;
-  }
+  };
 
+  /**
+   * Parse text node for template variables.
+   * @param {HtmlElement} node the node to parse
+   * @returns {Object} a template variable node
+   *          {HtmlElement} the node
+   *          {markup} the template markup
+   */
   Bind.prototype.parseTextNode = function (node) {
     var nodeValue = node.nodeValue,
         regEx     = /{{(\w*)}}/g,
         match,
         modelName,
         modelString,
-        datum = {};
+        datum;
 
-    while (match = re.exec(value)) {
+    while (match = regEx.exec(nodeValue)) {
       modelName   = match[1];
       placeholder = match[0];
 
       datum = {
         node: node,
-        originalValue: node.nodeValue,
-        placeholder: placeholder,
-        name: modelName
+        markup: node.nodeValue
       };
 
-      node.nodeValue = node.nodeValue.replace(placeholder, '');
     }
 
     return datum;
-  }
+  };
+
+  /**
+   * Render a template
+   * @param {Array} template the template node data array
+   * @param {Object} context the data context with values for the template variables
+   */
+  Bind.prototype.render = function (template, context) {
+    var updatedNodes =  [];
+
+    template.forEach(function (node) {
+      node.node.nodeValue = this.renderTemplateMarkup(node.markup, context);
+    }, this);
+  };
+
+  /**
+   * Update a node with new variable values
+   * @param {String} templateMarkup the original markup for the node
+   * @param {Object} context the data context with values for the template variables
+   */
+  Bind.prototype.renderTemplateMarkup = function (templateMarkup, context) {
+    var output = '',
+        orig = templateMarkup,
+        startPos = null,
+        endPos = null,
+        c,
+        i,
+        len,
+        variableName;
+
+    for (i = 0, len = orig.length; i < len; i++) {
+      c = orig[i];
+
+      if (c === '{' && orig[i - 1] !== '{') {
+        startPos = i;
+      }
+
+      if (c === '}' && orig[i + 1] !== '}') {
+        endPos = i;
+      }
+
+      if (startPos === null) {
+        output += c;
+      }
+
+      if (startPos !== null && endPos !== null) {
+        variableName = orig.substring(startPos + 2, endPos - 1);
+
+        if (context[variableName]) {
+          output += context[variableName];
+        }
+
+        startPos = null;
+        endPos = null;
+      }
+
+    }
+
+    return output;
+  };
 
 }(window));
--- tests/bind.spec.js
+++ tests/bind.spec.js
@@ -1,8 +1,54 @@
 /**
  * @venus-library mocha
+ * @venus-include ../bind.js
  * @venus-fixture bind.fixture.html
  */
 describe('bind library', function () {
 
+  describe('parseNode', function () {
+    var node, result;
+
+    before(function () {
+      node = document.getElementById('fixture-1');
+      result = window.bind.parseNode(node);
+    });
+
+    it('should parse two nodes containing variables', function () {
+      expect(result.length).to.be(2);
+    });
+
+    it('should parse node', function () {
+      expect(result[0].node).to.be.a('object');
+    });
+
+    it('should parse template markup', function () {
+      expect(result[0].markup).to.be('Hi {{name}}, how does it feel to be {{age}} years old?');
+    });
+
+  });
+
+  describe('create template', function () {
+    var node, template;
+
+    beforeEach(function () {
+      node = document.getElementById('fixture-1');
+      template = window.bind.template(node);
+    });
+
+    it('should be a function', function () {
+      expect(template).to.be.a('function');
+    });
+
+    it('should render inner text', function () {
+      template({name: 'Seth', age: 78});
+      expect(node.innerText).to.be('Hi Seth, how does it feel to be 78 years old?');
+    });
+
+    it('should render attributes', function () {
+      template({name: 'Seth', age: 78});
+      expect(node.className).to.be('Seth');
+    });
+
+  });
 
 });
